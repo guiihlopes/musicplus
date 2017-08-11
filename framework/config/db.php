7e7511a1887da1f592f3382053a38cdd4eb5ee6f
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-08-11 01:02:47 -0300

    Inclui sql e alterei configuracao do banco de dados

--- framework/config/db.php
+++ framework/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost:3306;dbname=musicplus',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
