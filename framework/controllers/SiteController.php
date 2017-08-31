d9e222d771d5eb753144237fb4bbd674568a394d
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-08-30 22:48:07 -0300

    Remove links de about e contact e sua respectiva controller

--- framework/controllers/SiteController.php
+++ framework/controllers/SiteController.php
@@ -98,32 +98,4 @@ class SiteController extends BaseController
 
         return $this->goHome();
     }
-
-    /**
-     * Displays contact page.
-     *
-     * @return Response|string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
 }
